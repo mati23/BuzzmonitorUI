2ab4fb6924931ac021f4b22ad298b924964a83a7
mati23 <mateusarruda@alu.ufc.br>
2018-10-12 23:13:49 -0300

    Primeira Parte

--- js/doughnutChart.js
+++ js/doughnutChart.js
@@ -1,12 +1,155 @@
-new Chart(document.getElementById("doughnut-chart"), {
+var context1 = $('#doughnut-chart1');
+var context2 = document.getElementById("doughnut-chart2");
+var context_month_usage = $('#chart_month_usage');
+
+var chart1 = new Chart(context1, {
     type: 'doughnut',
     data: {
-    
+
       datasets: [
         {
           label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          backgroundColor: ["#2F8EE3", "#3FA745","#383C42"],
+          data: [100,100,1000]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
+function atualizaChart1(old_chart1){
+
+  colors = ["#FBC21A", "#3FA745", "#aa00ff", "#00695c", "#388e3c","#ef6c00","#37474f","#ad1457", "#4527a0","#6a1b9a"]
+  old_chart1.destroy();
+  var new_quota =checaUnlimited(); //parseInt($('#input_chart1').val());
+  var vector = [];
+  for (var i = 0; i < new_quota; i++) {
+    vector[i] = 100;
+  }
+  var chart1 = new Chart(context1, {
+      type: 'doughnut',
+      data: {
+
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: colors,
+            data: vector
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: false,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+  });
+
+}
+
+var chart2 = new Chart(context2, {
+    type: 'doughnut',
+    data: {
+
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#6C747C"],
+          data: [2478,5267,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
+function atualizaChart2(old_chart2) {
+
+  old_chart2.destroy();
+  var new_quota = parseFloat($('#input_chart2').val());
+  var chart2 = new Chart(document.getElementById("doughnut-chart2"), {
+      type: 'doughnut',
+      data: {
+
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#D4384A", "#3BB226","#31373F","#6C747C","#6C747C","#6C747C","#6C747C","#6C747C","#6C747C","#6C747C","#6C747C","#6C747C"],
+            data: [new_quota,new_quota-(new_quota/2),new_quota-(new_quota/2),new_quota,new_quota,new_quota,new_quota,new_quota,new_quota,new_quota,new_quota,new_quota]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: false,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+  });
+
+}
+
+new Chart(document.getElementById("doughnut-chart3"), {
+    type: 'doughnut',
+    data: {
+
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#6C747C"],
+          data: [2478,5267,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
+new Chart(document.getElementById("doughnut-chart4"), {
+    type: 'doughnut',
+    data: {
+
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#6C747C"],
+          data: [2478,5267,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
+
+
+var month_usage = new Chart(context_month_usage, {
+    type: 'doughnut',
+    data: {
+
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#2F8EE3", "#3FA745","#383C42"],
+          data: [100,100,1000]
         }
       ]
     },
---
+++ js/pageLogic.js
@@ -0,0 +1,31 @@
+function checaUnlimitedPerMonth(){
+  var checkbox = document.getElementById("posts_collected");
+
+  if (checkbox.checked == false) {
+    document.getElementById("input_chart2").classList.toggle('disabled');
+
+  }
+}
+
+$("#posts_collected").change(function(){
+  if(this.checked) {
+    document.getElementById("input_chart2").disabled=false;
+
+  }else {
+    document.getElementById("input_chart2").disabled=true;
+  }
+});
+
+
+
+function checaUnlimited(){
+  var checkbox = document.getElementById("switch_projects_collected");
+  if(checkbox.checked == false) {
+    //$(this).prop("checked", returnVal);
+    if ($('#input_chart1').val()>5) {
+      alert("Only less or equals to 5");
+      return 5;
+    }
+  }
+  return parseInt($('#input_chart1').val());
+}
